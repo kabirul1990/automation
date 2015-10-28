fc72abe9d5d0f339cb644c7e43c73421f89ad7b8
kabirul1990 <kabirul1990@gmail.com>
2015-10-28 16:57:18 +0530

    Update SampleTest.java

--- SampleProject/src/test/java/regression/SampleTest.java
+++ SampleProject/src/test/java/regression/SampleTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class SampleTest {
 	@BeforeClass
 	public void beforeClass() {
-        System.out.println("Before Class");
+        System.out.println("Before Class123");
 	}
 	@AfterClass
     public void afterClass(){
